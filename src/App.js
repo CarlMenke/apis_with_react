ccd1c4ac7ec1d8fe1c19c766da6ea37328de349c
carlmenke <carlmenke13@gmail.com>
2022-08-31 09:46:02 -0700

    added readme instructions and base code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css'
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [movies, setMovies] = useState([])
+  const [selectedMovie, setSelectedMovie] = useState(null) // this should be a movie once you select one
+
+  return <div></div>
 }
-
-export default App;
